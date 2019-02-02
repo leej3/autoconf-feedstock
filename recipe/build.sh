b4d167656ba433ae5ce052fbf93674765fdf4820
Marius van Niekerk <marius.v.niekerk@gmail.com>
2019-02-01 22:18:16 -0500

    attempt to use a more modern version of config.guess

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+if [[ ${target_platform} == linux-ppc64le ]]; then
+    pushd
+    rm config.guess
+    curl -o config.guess http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD
+    popd
+fi
+
 ./configure --prefix=${PREFIX}        \
             --libdir=${PREFIX}/lib    \
             --build=${BUILD}          \
