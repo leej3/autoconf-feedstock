94dc5cc9dad3d02b226961b32a518c4271a2b776
Isuru Fernando <isuruf@gmail.com>
2020-11-11 10:28:42 -0600

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,7 +14,7 @@ popd
             PERL='/usr/bin/env perl'
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
-if [[ "$CONDA_BUILD_CROSS_COMPILATION" != "1" ]]; then
+if [[ "$build_platform" != "$target_platform" ]]; then
   make check || { cat tests/testsuite.log; exit 1; }
 fi
 make install
