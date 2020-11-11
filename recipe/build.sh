29c22d7ef1cd198d36463a889b895905eb468929
Isuru Fernando <isuruf@gmail.com>
2020-11-11 09:18:16 -0600

    fix pushd for ppc64le

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [[ ${target_platform} == linux-ppc64le ]]; then
-    pushd
+    pushd build-aux
     rm config.guess
     curl -o config.guess http://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD
     popd
