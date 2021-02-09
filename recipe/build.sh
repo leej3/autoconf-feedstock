387ab58327bb6674fb25d3b75083a70e1d03cfe6
Ilan Schnell <ilanschnell@gmail.com>
2021-02-09 17:26:08 -0600

    merging Paul's fixes

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,20 +1,27 @@
 #!/bin/sh
 
-pushd build-aux
-  rm config.guess
-  curl -o config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
-  rm config.sub
-  curl -o config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
-popd
+if [[ ! $BOOTSTRAPPING == yes ]]; then
+  pushd build-aux
+    rm config.guess
+    curl -o config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
+    rm config.sub
+    curl -o config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
+  popd
+fi
 
-./configure --prefix=${PREFIX}        \
-            --libdir=${PREFIX}/lib    \
-            --build=${BUILD}          \
-            --host=${HOST}            \
-            PERL='/usr/bin/env perl'
+./configure \
+    --prefix=${PREFIX}        \
+    --libdir=${PREFIX}/lib    \
+    --build=${BUILD}          \
+    --host=${HOST}            \
+    PERL='/usr/bin/env perl'
 
 make -j${CPU_COUNT} ${VERBOSE_AT}
-if [[ "$build_platform" == "$target_platform" ]]; then
-  make check || { cat tests/testsuite.log; exit 1; }
+
+if [[ ! $BOOTSTRAPPING == yes ]]; then
+  if [[ "$build_platform" == "$target_platform" ]]; then
+    make check || { cat tests/testsuite.log; exit 1; }
+  fi
 fi
+
 make install
