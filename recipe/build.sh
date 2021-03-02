12ee1133aa5f12d4e3f65bb7f1dc2cd02c9b0ba5
Ilan Schnell <ilanschnell@gmail.com>
2021-03-02 16:39:41 -0600

    add M4 environment variable and patch dependency in non-bootstrapping mode

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,9 @@ if [[ ! $BOOTSTRAPPING == yes ]]; then
     curl -o config.guess 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.guess;hb=HEAD'
     rm config.sub
     curl -o config.sub 'https://git.savannah.gnu.org/gitweb/?p=config.git;a=blob_plain;f=config.sub;hb=HEAD'
-  popd
+    popd
+else
+    export M4=$(which m4)
 fi
 
 ./configure \
